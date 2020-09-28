7b14578472b7fcaee7fd7b8b06d40889722e70a3
Vince <vincent_field@foxmail.com>
2020-09-28 23:27:45 +0800

    refresh scop

--- git-blog-fetcher/src/main/java/com/uetty/nacos/blogfetcher/controller/FetcherController.java
+++ git-blog-fetcher/src/main/java/com/uetty/nacos/blogfetcher/controller/FetcherController.java
@@ -1,11 +1,13 @@
 package com.uetty.nacos.blogfetcher.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/git")
+@RefreshScope
 public class FetcherController {
 
     @Value(value = "${app.tip:}")
