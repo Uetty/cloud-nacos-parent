de220959b6a5f102dd3c0f62a2ea00dfc4652a28
Vince <vincent_field@foxmail.com>
2020-11-03 16:46:55 +0800

    update

--- nacos-console/bin/startup.sh
+++ nacos-console/bin/startup.sh
@@ -9,11 +9,10 @@ echo $BASE_DIR
 JAVA_OPT="-Xms200m -Xmx512m"
 JAVA_OPT="$JAVA_OPT -XX:-OmitStackTraceInFastThrow -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${BASE_DIR}/logs/java_heapdump.hprof"
 JAVA_OPT="$JAVA_OPT -Dnacos.home=${BASE_DIR}"
-JAVA_OPT="$JAVA_OPT -jar ${BASE_DIR}/target/$SERVER.jar"
+JAVA_OPT="$JAVA_OPT -jar ${BASE_DIR}/lib/$SERVER.jar"
 JAVA_OPT="$JAVA_OPT --spring.config.location=${CUSTOM_SEARCH_LOCATIONS}"
 JAVA_OPT="$JAVA_OPT --logging.config=${BASE_DIR}/conf/nacos-logback.xml"
 JAVA_OPT="$JAVA_OPT --server.max-http-header-size=524288"
 
 
-echo "java ${JAVA_OPT} >> ${BASE_DIR}/logs/start.out 2>&1 &"
-nohup java ${JAVA_OPT} >> ${BASE_DIR}/logs/start.out 2>&1 &
+nohup java ${JAVA_OPT} >> /dev/null 2>&1 &
